83f9462e1f7eaba926ef476faa87ae13bfe59f84
SopnilSwapon <sopnilswapon1@gmail.com>
2025-08-11 22:03:14 +0600

    created initial files & install basic packange

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+
+import Login from "./components/Login";
+function App() {
+
+  return (
+    <>
+    <div className="flex min-h-screen bg-black w-full justify-center items-center border-2 border-cyan-900">
+
+      <Login/>
+    </div>
+    </>
+  )
+}
+
+export default App
---
+++ src/components/Login.tsx
@@ -0,0 +1,47 @@
+import axios from "axios";
+import { useState } from "react";
+
+export default function Login() {
+  const [values, setValues] = useState({email: "", password: ""});
+
+  const handleSubmit = (e: { preventDefault: () => void; }) => {
+    e.preventDefault();
+    axios.post("http://localhost:3000/auth/adminlogin")
+    .then(res => console.log(res))
+    .catch(err => console.log(err));
+    console.log('clicked', values)
+
+  }
+
+  return (
+    <div className="flex min-h-svh flex-col items-center justify-center">
+      <form onSubmit={handleSubmit} className="bg-gray-700 rounded-2xl p-6 min-w-96 flex flex-col gap-4">
+        <h1 className="text-3xl text-center text-white font-semibold ">Login Page</h1>
+        <div>
+
+  
+                      <label className="block text-sm/6 font-medium text-white">Username</label>
+            <div className="flex items-center rounded-md bg-white/5 pl-3 outline-1 -outline-offset-1 outline-white/10 focus-within:outline-2 focus-within:-outline-offset-2 focus-within:outline-indigo-500">
+
+              <input id="email"
+              onChange={(e) => setValues({...values, email:e.target.value})}
+              type="email" name="email" placeholder="your email" className="bg-transparent py-1.5 text-base text-white placeholder:text-gray-500 focus:outline-none sm:text-sm/6" />
+            </div>
+        </div>
+        <div>
+
+                      <label className="block text-sm/6 font-medium text-white">Password</label>
+            <div className="flex items-center rounded-md bg-white/5 pl-3 outline-1 -outline-offset-1 outline-white/10 focus-within:outline-2 focus-within:-outline-offset-2 focus-within:outline-indigo-500">
+
+              <input id="password"
+                onChange={(e) => setValues({...values, password:e.target.value})}
+              type="password" name="password" placeholder="your password" className="block min-w-0 grow bg-transparent py-1.5 pr-3 pl-1 text-base text-white placeholder:text-gray-500 focus:outline-none sm:text-sm/6" />
+            </div>
+        </div>
+      <button type="submit" className="px-8 py-1.5 rounded-md bg-gray-800 text-gray-100">Sign In</button>
+
+      </form>
+    </div>
+  );
+}
+
